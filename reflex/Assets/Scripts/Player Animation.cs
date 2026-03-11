ea3fa637c056de3ed2fa77e25a46ebb31c37fbc2
Packhat <carlojay432@gmail.com>
2026-03-11 23:01:23 +0800

    [Fix]Walk Animation fixed when attacking

--- reflex/Assets/Scripts/Player Animation.cs
+++ reflex/Assets/Scripts/Player Animation.cs
@@ -5,27 +5,16 @@ public class PlayerAnimation : MonoBehaviour
 {
     [SerializeField] private Animator playerAnim;
     [SerializeField] private PlayerMovementManagement playerMovement;
-    private bool isRunning = false;
-    private bool lastState = false;
-
+   
     private void Update()
     {
         MoveState();
     }
 
-    private void MoveState()
+   private void MoveState()
     {
-        isRunning = playerMovement.moveInput.magnitude > 0.1f;
-        if (isRunning && !lastState)
-        {
-            playerAnim.SetTrigger("run");
-            lastState = true;
-        }
-        else if(!isRunning && lastState)
-        {
-            playerAnim.SetTrigger("runToIdle");
-            isRunning = false;
-            lastState = false;
-        }
-    }   
+        bool isRunning = playerMovement.moveInput.magnitude > 0.1f;
+        playerAnim.SetBool("isRunning", isRunning);
+    }
+
 }
